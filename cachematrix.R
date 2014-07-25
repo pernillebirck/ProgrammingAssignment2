43f1928ef4567e67b951a9ab917bdc38043f494b
pernille Birck Langer <pernillebirck@me.com>
2014-07-25 15:50:34 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions makes caches of matrixes and inverse them
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {  ## this function makes a cache matrix
+  i <- NULL    ## where i is the values of the inversed matrix
+  setvalues <- function(y) {    ## this function resets the values of i 
+    x <<- y
+    i <<- NULL
+  }
+  getvalues <- function() x ## this function gets the assigned values of x and returns them
+  setcache <- function(inversed) i <<- inversed ## this function cached i to setinversedvalues
+  getcache <- function() i   ## this function gets the cached values of i and returns them
+  list(set = setvalues, get = getvalues, 
+       setinversedvalues = setcache,
+       getinversedvalues = getcache)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {      ## this function either returns an old inverse matrix, or calculates a new inverse matrix if new parameters are set
+  i <- x$getinversedvalues()   ## where i is the values of the inverse matrix
+  if(!is.null(i)){    ## when a new i is not set the function gets the cached i
+    message("getting cached data") ## prints message
+    return(i) ## returns the cached i
+  }
+  data <- x$getvalues() ## gets the newly set values and assigns it to data
+  i <- data %*% solve(data) ## calculates the inverse matrix of data
+  x$setinversedvalues(i) ## sets the newly calculated inverse matrix to x
+  i ## returns the newly calculated inverse matrix
 }
